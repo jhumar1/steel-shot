0b21cd9e1c27c74f632ef55b6ce6c550b4f1a87e
Jhumar <101704823+jhumar1@users.noreply.github.com>
2022-06-01 14:33:12 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,93 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+      <p > A <a href="https://www.gritblast.co.in/"> Sand Blasting Machine </a> For Sale is an essential part of restoring your surfaces. The
+process uses high-pressure air to eliminate surface-damaging precipitates. It is the ideal solution
+for difficult-to-reach areas, as it is less invasive than sandpaper, and is more economical. These
+machines can be easily moved from one site to another without the need for complex machinery.
+Here are some tips for buying a Sand Blasting Machine for Sale:
+Sand Blasting Machines are versatile and useful tools. A small machine can remove the edges
+of small rubber products, plastic alloys, and complex castings without damaging the surface.
+Compared to sandpaper, a Sand Blasting Machine is more effective in preparing surfaces for
+polishing and buffing. Moreover, it reduces labor costs. The main benefit of a Sand-Blasting
+Machine For Sale is its low cost and versatility.
+When selecting a Sand-Blasting Machine for Sale, make sure that the machine is designed for
+the specific work you need to perform. Most of the machines are made of aluminum, and you
+
+can find models that have up to eight blast nozzles. Each nozzle produces a stream of fine-
+grained sand at a rate of 26 cfm. The total volume of air that is discharged into the air is clean
+
+and dust-free.
+
+ </p>
+
+<h2> Sand Blasting Machine For Sale  </h2>
+
+ <p> A <a href="gritblast.co.in"> sand blasting machine </a> is used in industrial applications for removing old paint, rust, and
+oxidation. The process is used to clean up a surface and is a cost-effective alternative to
+mechanical grinding. The sandblasting machine is powered by a good air compressor, which
+produces up to 800 litres of air per minute. It can remove three to four square feet of material,
+and requires an electric connection of at least 12 volts.
+There are two basic types of media to
+choose from: round media and angular
+media.<br><br> Round media is better for removing
+paint. An angular media is preferred for
+securing surfaces and imparting
+compressive stress. Both types can be
+used in a variety of industrial applications.
+Many manufacturers also offer eco-friendly
+solutions for environmental concerns.
+Plant-based media is an affordable
+alternative.<br><br><br>
+
+</p>
+ <h2 >Steel Shots </h2>
+ <p >    
+Steel shot is the most widely used method in sand & shot blasting industries for cleaning, stripping, and improving a metal surface. The grade or size of the steel shot will determine the finishing done on the surface of the metal.
+
+A smaller <a href="https://www.shotblastingmachines.in/steel-shot"> steel shots </a> exporter will appear on a smoother and more polished surface. 
+A larger <a href="https://www.sandblastingmachine.in/steel-shots/"> steel shots </a> exporter will clean more aggressively but produce a more 
+uneven surface. Both breaths of air-powered and wheel blast machine can be 
+used to stimulate the steel shot manufacturer in India onto the surface of 
+the part. Due to the hardness and density of steel shot manufacturer in India,
+ it can be reused as much as 3000 times before replacement. Since steel shot 
+supplier does not use an abrasives process, a minimal amount of dust is created 
+in the blasting process.
+
+</p>
+
+ <p >  
+ <h2 >Steel Shots </h2>
+<a href="https://www.sandblastingmachine.in/steel-shots/"> Steel shots </a> is spherical metallic abrasive media produced by 
+the molten steel by means of atomizing process. Metalworking 
+Industries are the main users of Steel Shots such as ferrous and 
+non-ferrous foundries, steel mills, forge shops and the metal fabricators, 
+sand removal and to improve the metal surface preparation etc. since 
+its process is not harmful, hence steel shot produces the least level of 
+dust. Blast cleaning with steel abrasive media is an important operation
+ at various stages of metal production. There are different varieties 
+and sizes of the steel shot which gives extra ordinary results on the 
+metal surface. It produces the most economical abrasive media which has 
+recycling capacity of more than 30 times due to its robustness and high 
+durability. A larger steel shot vigorously cleans a rougher surface. To 
+accelerate steel shot on the surface, both wheel blasting machine and air 
+powdered system is used. Steel shot produces the least dust because its process
+ is not harsh. Steel shots are used to clean heavy parts of metals such as engine 
+turbine blades, crankshafts and heavy-duty spring, etc. Low carbon steel shot has a
+ low hardness in comparison to high carbon steel shot. Because of its microstructure 
+it resists impact, while the moment it suddenly scatters/dissipate and turns into fine 
+powdered dust. It is even used for flash descaling. Since low carbon steel shot is 
+softer in nature, it 
+is not possible to crush it into grits, therefore it is used less. Steel shots are 
+made from steel scrap. The furnace is an electric furnace. Steel scrap is melted in 
+this furnace and then being atomized into the steel shots by water jet.
+
+
+</p>
+
+        
+    
+    </main>
 
       <Footer />
     </div>
